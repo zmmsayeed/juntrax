c95b7a6ac919855894a87f4f1bad1b1cb2500c51
Zimam Sayeed <zmmsayeed@gmail.com>
2019-04-27 01:18:07 +0530

    Added tabs

--- js/custom.js
+++ js/custom.js
@@ -24,3 +24,17 @@ document.getElementById('button').addEventListener('click', function() {
 document.querySelector('.close').addEventListener('click', function() {
   document.querySelector('.bg-modal').style.display = 'none';
 });
+
+function openBills(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
